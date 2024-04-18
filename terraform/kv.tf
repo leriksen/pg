47f2fad83ab383f0d6d6c47737463899660506e2
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-04-18 14:25:31 +1000

    store secrets

--- terraform/kv.tf
+++ terraform/kv.tf
@@ -6,3 +6,15 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled = false
 }
+
+resource "azurerm_key_vault_secret" "pguser" {
+  key_vault_id = azurerm_key_vault.kv.id
+  name         = "pguser"
+  value        = var.pguser
+}
+
+resource "azurerm_key_vault_secret" "pgpassword" {
+  key_vault_id = azurerm_key_vault.kv.id
+  name         = "pgpassword"
+  value        = var.pgpassword
+}
