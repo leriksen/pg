b1d4208f3b622fa4ef6ec5b9b5b816565891a852
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-05-02 18:05:03 +1000

    archive package

--- helm/helm.sh
+++ helm/helm.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-helm package --version 0.1.0 --app-version 20240427.7 postgres-secrets
-helm upgrade --namespace dev --install --values postgres-secrets/values.yaml --wait --install --create-namespace --timeout 30s postgres postgres-secrets-0.1.0.tgz --version 0.1.0
+helm package --version 0.1.0 --app-version 20240427.7 postgres
+helm upgrade --namespace dev --install --values postgres/values.yaml --wait --install --create-namespace --timeout 30s postgres postgres-0.1.0.tgz --version 0.1.0
