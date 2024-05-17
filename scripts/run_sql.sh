0ebd647d2275fe2e296a5a3d96efde61b49759e6
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-05-17 16:54:26 +1000

    move to a job

--- scripts/run_sql.sh
+++ scripts/run_sql.sh
@@ -13,7 +13,8 @@ while IFS= read -r line || [ -n "${line}" ]; do
   if [[ "${line}" =~ ^\s*# || "${line}" =~ ^\s*$ ]]; then
     echo "skipping"
   elif [[ "${line}" =~ create_database ]]; then
-    psql --dbname=template1 --file "${line}" "${secrets}"
+    echo "psql --file ${line} ${secrets}"
+    psql --dbname=template1 --file "${line}" --set=dbname=personify
   else
     psql --dbname personify --file "${line}" "${secrets}"
   fi
