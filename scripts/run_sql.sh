0560cde13830efc15831720707cfb354b1548bbc
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-05-23 23:59:22 +1000

    testing of var setting syntax

--- scripts/run_sql.sh
+++ scripts/run_sql.sh
@@ -2,21 +2,23 @@
 
 set -euo pipefail
 
+echo "running $(psql --version)"
 input="${1}"
 
 echo "reading file ${input}"
 
-secrets=$(jq -r 'to_entries|map("--set=\(.key)=\(.value|tostring)")| join(" ")' "${2}")
+#secrets=$(jq -r 'to_entries|map("-v \(.key)=\"\(.value|tostring)\"")| join(" ")' "${2}")
 
 while IFS= read -r line || [ -n "${line}" ]; do
   echo "${line}"
   if [[ "${line}" =~ ^\s*# || "${line}" =~ ^\s*$ ]]; then
     echo "skipping"
   elif [[ "${line}" =~ create_database ]]; then
-    echo "psql --file ${line} ${secrets}"
-    psql --dbname=template1 --file "${line}" --set=dbname=personify
+    echo "psql -d postgres --file ${line} -v dbname=personify"
+    psql -d postgres --file "${line}" -v dbname=personify
   else
-    psql --dbname personify --file "${line}" "${secrets}"
+    echo "psql --dbname personify --file ${line} -v cashflow_admin_password=secret_password"
+    psql --dbname personify --file "${line}" -v cashflow_admin_password=secret_password
   fi
 done < "$input"
 
--- sql/05_users.sql
+++ sql/05_users.sql
@@ -1,3 +1,5 @@
 SET search_path = sample;
 
-CREATE USER cashflow_admin WITH PASSWORD ':cashflow_admin_password';
+CREATE USER cashflow_admin WITH PASSWORD :'cashflow_admin_password';
+GRANT CONNECT ON DATABASE personify TO cashflow_admin;
+GRANT ALL PRIVILEGES ON DATABASE personify TO cashflow_admin;
