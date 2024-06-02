7e9b5f14bd12e89510c9edeca10ccb6285bee419
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-06-02 19:32:36 +1000

    check dir

--- migrations/migrations/sqls/20240602071129-admin-user-up.sql
+++ migrations/migrations/sqls/20240602071129-admin-user-up.sql
@@ -1,7 +1,7 @@
 SET search_path = sample;
 
-CREATE USER personify_read_user WITH PASSWORD '${PERSONIFY_READ_USER}'
+CREATE USER personify_read_user WITH PASSWORD 'env_password';
 
-/* make sure to reset search path to public, so migration can be registered */
+-- make sure to reset search path to public, so migration can be registered
 SET search_path = public;
 
