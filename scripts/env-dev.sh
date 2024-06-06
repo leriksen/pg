4ddfe96b9cd6fc5027ff6dbf5ef3eb7234f8d756
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-06-06 14:37:45 +1000

    local testing

---
+++ scripts/env-dev.sh
@@ -0,0 +1,14 @@
+export TF_WORKSPACE="pg"
+export TF_VAR_pguser="psqladmin"
+export TF_VAR_pgpassword="$(cat .pgpass)"
+export AZDO_ORG_SERVICE_URL="https://dev.azure.com/leiferiksenau"
+export AZDO_PERSONAL_ACCESS_TOKEN="$(cat .pat)"
+export PGPASSWORD=$(cat .pgpass)
+export PGUSER=psqladmin
+export PGPORT=5432
+export PGHOST=localhost
+export PGSSLMODE=disable
+export MIGRATION_DIRECTION=up
+export INIT_MIGRATE=true
+export DRY_RUN=""
+export ADMIN_PASSWORD=example
